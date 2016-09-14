561e05bf56b6d63990c9970c1fee2620f8023f48
Maia Stone <maiadov@gmail.com>
2016-09-14 13:12:57 -0600

    Set file structure

---
+++ test/index.js
@@ -0,0 +1,1 @@
+require ('tile-test.js');
---
+++ test/tile-test.js
@@ -0,0 +1,14 @@
+describe function makeTile
+
+context game start
+
+it should be a function
+
+it should have a instantiate the object tile
+
+it should take the first argument and set the x property of the instantiated object
+
+it should take the second argument and set the y property to the instantiated object
+
+-- how does prototype fit in?
+we want each tile to have the same height and width
