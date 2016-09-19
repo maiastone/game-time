24e33e05d0ae33360f91f8e91ad45db93e8bd4ce
Maia Stone <maiadov@gmail.com>
2016-09-18 22:44:58 -0600

    refactor file structure and tests

--- test/grid-test.js
+++ test/grid-test.js
@@ -1,62 +1,23 @@
 const assert = require('chai').assert;
-const Grid = require('../lib/grid');
 
-
-describe('Grid', function () {
-
-  it('should be a function', function(){
-    assert.isFunction(Grid);
+describe('allTiles', function() {
+  it('should be set to an empty array', function(){
+    var allTiles = [];
+    assert.isArray(allTiles);
   });
 
-  it('should instantiate an object', function(){
-    var grid = new Grid();
-    assert.isObject(grid);
+  it('should set up tile relationships automatically', function(){
+
   });
 
-  it.skip('should be able to create a new tile', function() {
-    //some function that calls a new tile
-    //maybe passes in some data and position
+  it.skip('can create a row', function() {
+
   });
+});
 
-  it.skip('should have a position', function() {
-    //verify that tile.position is what you think it should be
-  });
-
-  it.skip('should have the data we assigned to it', function() {
-    //verify that when we create a new tile with data, tile.data is what we think it should be
-    // var tile = new Tile(2, '#c14')
-    // assert.equal(tile.data, 2)
-  });
-
-  //for the grid
-    it.skip('can create a row', function() {
-
-    });
-    
-
-  it.skip('should move up when TileMoveUp is called', function() {
-    //var tile = new tile (2, #c16)
-    //Grid.tilesUp()
-    //assert.equal(tile.postion, #c4)
-  });
-
+describe('smush', function(){
   it.skip('should move the tiles left');
 
   it.skip('should move the tiles right');
 
-
-  it.skip('should combine with another tile if that tile has the same data');
-    //var tile1 = new tile(data,position)
-    //var tile2 = new Tile(data,postiion)
-    //Grid.moveRight()
-    //in the rightmost position there is 1 tile whose data is double the original
-    //assert.equal (#c16, tile)
-    //asser.equal (tile4.data, tile1.data + tile2.data)
-
-  it.skip('should NOT combine with another tile if the data does not match');
 });
-//
-// describe('board', function(){
-//     it('should have a method called "leftArrow"',function(){
-//         var board = new Grid ()
-//       })
--- test/tile-test.js
+++ test/tile-test.js
@@ -1,25 +1,59 @@
 const assert = require('chai').assert;
 const Tile = require('../lib/tile');
-// describe function makeTile
-// context game start
-// it should be a function
-//
-// it should have a instantiate the object tile
-//
-// it should take the first argument and set the x property of the instantiated object
-//
-// it should take the second argument and set the y property to the instantiated object
-//
-// -- how does prototype fit in?
-// we want each tile to have the same height and width
+
+
 describe('Tile', function() {
 
   it('should be a function', function(){
     assert.isFunction(Tile);
   });
 
-  it.skip('it should have a instantiate the object tile', function(){
+  it('should instantiate the object tile', function(){
     var tile = new Tile();
     assert.isObject(tile);
   });
+
+  it('should have default attributes', function() {
+    assert.equal(this.data, null);
+    assert.equal(this.next, null);
+    assert.equal(this.above, null);
+    assert.equal(this.left, null);
+    assert.equal(this.below, null);
+  });
+
+  it('should be able to create a new tile', function() {
+    assert.isFunction(Tile.prototype.start);
+  });
+    //some function that calls a new tile
+    //maybe passes in some data and position
+    it.skip('should have a position', function() {
+      //verify that tile.position is what you think it should be
+    });
+
+    it.skip('should have the data we assigned to it', function() {
+      //verify that when we create a new tile with data, tile.data is what we think it should be
+      // var tile = new Tile(2, '#c14')
+      // assert.equal(tile.data, 2)
+    });
+
+
+
+    it.skip('should move up when TileMoveUp is called', function() {
+      //var tile = new tile (2, #c16)
+      //Grid.tilesUp()
+      //assert.equal(tile.postion, #c4)
+    });
+
+
+
+    it.skip('should combine with another tile if that tile has the same data');
+      //var tile1 = new tile(data,position)
+      //var tile2 = new Tile(data,postiion)
+      //Grid.moveRight()
+      //in the rightmost position there is 1 tile whose data is double the original
+      //assert.equal (#c16, tile)
+      //asser.equal (tile4.data, tile1.data + tile2.data)
+
+    it.skip('should NOT combine with another tile if the data does not match');
+
 });
